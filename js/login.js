ddd80cba76fa607d09000cc264f9f94e58bb6b5d
JulianoLand <juliannojp@gmail.com>
2025-05-05 20:06:00 -0300

    começando a estilização

--- js/login.js
+++ js/login.js
@@ -1,25 +1,25 @@
 // /js/login.js
 
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
-e.preventDefault();
+    e.preventDefault();
 
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
     try {
         const response = await fetch('http://192.168.18.102:5000/api/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
         });
 
         const data = await response.json();
 
         if (response.ok) {
-        localStorage.setItem('token', data.token); // salva token
-        window.location.href = '/index.html'; // redireciona
+            localStorage.setItem('token', data.token); // salva token
+            window.location.href = '/index.html'; // redireciona
         } else {
-        alert(data.message || 'Erro ao logar');
+            alert(data.message || 'Erro ao logar');
         }
     } catch (err) {
         console.error('Erro:', err);
